1b13e1943741f9185ae09a24b7bba0fe1fd96337
Twivbas <1423228908@qq.com>
2020-07-20 23:54:47 +0800

    change css

--- 视频播放器/script.js
+++ 视频播放器/script.js
@@ -1,65 +1,65 @@
+
 const video = document.getElementById('video');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-// Play & pause video
+//play & pause video
 function toggleVideoStatus() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
 }
 
-// update play/pause icon
+//update play & pause icon
 function updatePlayIcon() {
-  if (video.paused) {
-    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
-  } else {
-    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
-  }
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
 }
 
-// Update progress & timestamp
+//update progress & timestamp
 function updateProgress() {
-  progress.value = (video.currentTime / video.duration) * 100;
+    progress.value = (video.currentTime / video.duration) * 100;
 
-  // Get minutes
-  let mins = Math.floor(video.currentTime / 60);
-  if (mins < 10) {
-    mins = '0' + String(mins);
-  }
+    //get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
 
-  // Get seconds
-  let secs = Math.floor(video.currentTime % 60);
-  if (secs < 10) {
-    secs = '0' + String(secs);
-  }
+    //get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
 
-  timestamp.innerHTML = `${mins}:${secs}`;
+    timestamp.innerHTML = `${mins}:${secs}`;
+    
 }
 
-// Set video time to progress
+//set video time to progress
 function setVideoProgress() {
-  video.currentTime = (+progress.value * video.duration) / 100;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-// Stop video
+//stop video
 function stopVideo() {
-  video.currentTime = 0;
-  video.pause();
+    video.currentTime = 0;
+    video.pause();
 }
 
-// Event listeners
+//event listener
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
-
 stop.addEventListener('click', stopVideo);
-
 progress.addEventListener('change', setVideoProgress);
