f4603d9be07d2c49c72931697272ec8f651da0f0
Twivbas <1423228908@qq.com>
2020-11-24 23:49:35 +0800

    finished photos.html

--- ch12/scripts/global.js
+++ ch12/scripts/global.js
@@ -165,6 +165,61 @@ function prepareInternalnav() {
   }
 }
 
+function showPic(whichpic) {
+  
+  if (!document.getElementById('placeholder')) return true;
+  let source = whichpic.getAttribute('href');
+  let placeholder = document.getElementById('placeholder');
+  placeholder.setAttribute('src', source);
+  if (!document.getElementById('description')) return false;
+  let text;
+  //完全用let代替var,有时会出错，变量定义了却未使用。当变量在两个不同的作用域内时 
+  if (whichpic.getAttribute('title')) {
+    text = whichpic.getAttribute('title');
+  } else {
+    text = '';
+  }
+  let description = document.getElementById('description');
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+
+  return false;
+}
+
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById('imagegallery')) return false;
+  let placeholder = document.createElement('img');
+  placeholder.setAttribute('id', 'placeholder');
+  placeholder.setAttribute('src', 'images/placeholder.gif');
+  placeholder.setAttribute('alt', 'my image gallery');
+  let description = document.createElement('p');
+  description.setAttribute('id', 'description');
+  let desctext = document.createTextNode('Choose an image');
+  description.appendChild(desctext);
+  let gallery = document.getElementById('imagegallery');
+  insertAfter(description, gallery);
+  insertAfter(placeholder,description);
+}
+
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById('imagegallery')) return false;
+  let gallery = document.getElementById('imagegallery');
+  let links = gallery.getElementsByTagName('a');
+  for (let i=0; i<links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this);
+    }
+  }
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
