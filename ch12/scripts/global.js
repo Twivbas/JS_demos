5e145130d0ce0bbf605997c5e0d98c0699ec6ece
Twivbas <1423228908@qq.com>
2020-11-25 17:59:53 +0800

    change live.html

--- ch12/scripts/global.js
+++ ch12/scripts/global.js
@@ -218,8 +218,77 @@ function prepareGallery() {
   }
 }
 
+function stripeTables() {
+  if (!document.getElementsByTagName) return false;
+  let tables = document.getElementsByTagName('table');
+  for (let i=0; i<tables.length; i++) {
+    let odd = false;
+    let rows = tables[i].getElementsByTagName('tr');
+    for (let j=0; j<rows.length; j++) {
+      if (odd == true) {
+        addClass(rows[j], 'odd');
+        odd = false;
+      } else {
+        odd = true;
+      }
+    }
+  }
+}
+
+function highlightRows() {
+  if (!document.getElementsByTagName) return false;
+  let rows = document.getElementsByTagName('tr');
+  for (let i=0; i<rows.length; i++) {
+    rows[i].oldClassName = rows[i].className;
+    rows[i].onmouseover = function() {
+      addClass(this, 'highlight');
+    }
+    rows[i].onmouseout = function() {
+      this.className = this.oldClassName;
+    }
+  }
+}
+
+function displayAbbreviations() {
+  if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+  let abbreviations = document.getElementsByTagName('abbr');
+  if (abbreviations.length < 1) return false;
+  let defs = new Array();
+  for (let i=0; i<abbreviations.length; i++) {
+    let current_abbr = abbreviations[i];
+    if (current_abbr.childNodes.length < 1) continue;
+    let definition = current_abbr.getAttribute('title');
+    let key = current_abbr.lastChild.nodeValue;
+    defs[key] = definition;
+  }
+  let dlist = document.createElement('dl');
+  for (key in defs) {
+    let definition = defs[key];
+    let dtitle = document.createElement('dt');
+    let dtitle_text = document.createTextNode(key);
+    dtitle.appendChild(dtitle_text);
+    let ddesc = document.createElement('dd');
+    let ddesc_text = document.createTextNode(definition);
+    ddesc.appendChild(ddesc_text);
+    dlist.appendChild(dtitle);
+    dlist.appendChild(ddesc);
+  }
+  if (dlist.childNodes.length < 1) return false;
+  let header = document.createElement('h3');
+  let header_text = document.createTextNode('Abbreviations');
+  header.appendChild(header_text);
+  let articles = document.getElementsByTagName('article');
+  if (articles.length == 0) return false;
+  let container = articles[0];
+  container.appendChild(header);
+  container.appendChild(dlist);
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
